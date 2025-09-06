d0b0b56017ef513061623ba85ff2c0d513ec6db8
Hanna Yohannes <hanyohmek@gmail.com>
2025-09-06 14:29:33 +0300

    tailwind download update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./0x02-tailwind-css/**/*.html",
+    "./0x02-tailwind-css/src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
